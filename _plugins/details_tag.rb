b1ef633e4478840353d970c573e9eb0947480f5b
yerassyl_s <YShalabaev@zakon.kz>
2019-11-01 09:33:39 +0600

    tag

--- _plugins/details_tag.rb
+++ _plugins/details_tag.rb
@@ -1,24 +1,23 @@
-module Jekyll
-    module Tags
-      class DetailsTag < Liquid::Block
+require "jekyll"
+
+class DetailsTag < Liquid::Tag
+
+def initialize(tag_name, markup, tokens)
+    super
+    @caption = markup
+end
+
+def render(context)
+    site = context.registers[:site]
+    converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
+    # below Jekyll 3.x use this:
+    # converter = site.getConverterImpl(::Jekyll::Converters::Markdown)
+    caption = converter.convert(@caption).gsub(/<\/?p[^>]*>/, '').chomp
+    body = converter.convert(super(context))
+    "<details><summary>#{caption}</summary>#{body}</details>"
+end
+
+Liquid::Template.register_tag "details", self
+end
+   
   
-        def initialize(tag_name, markup, tokens)
-          super
-          @caption = markup
-        end
-  
-        def render(context)
-          site = context.registers[:site]
-          converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
-          # below Jekyll 3.x use this:
-          # converter = site.getConverterImpl(::Jekyll::Converters::Markdown)
-          caption = converter.convert(@caption).gsub(/<\/?p[^>]*>/, '').chomp
-          body = converter.convert(super(context))
-          "<details><summary>#{caption}</summary>#{body}</details>"
-        end
-  
-      end
-    end
-  end
-  
-  Liquid::Template.register_tag('details', Jekyll::Tags::DetailsTag)
